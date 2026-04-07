5dd43c229f9e3d7db2abd964bfa1a6c4af829a21
abhignareddie <abhireddy2709@gmail.com>
2026-04-07 12:22:19 +0530

    Remove default value for HF_TOKEN

--- inference.py
+++ inference.py
@@ -5,7 +5,7 @@ from grader import grade
 
 API_BASE_URL = os.getenv("API_BASE_URL", "https://api.openai.com/v1")
 MODEL_NAME = os.getenv("MODEL_NAME", "gpt-4o-mini")
-HF_TOKEN = os.getenv("HF_TOKEN", "dummy-token")
+HF_TOKEN = os.getenv("HF_TOKEN")
 LOCAL_IMAGE_NAME = os.getenv("LOCAL_IMAGE_NAME")
 
 client = OpenAI(api_key=HF_TOKEN, base_url=API_BASE_URL)
